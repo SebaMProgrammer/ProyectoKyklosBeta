6959f824d8ee231fde41e3b056a0249e190768e0
SebaMProgrammer <monjes.sebastian@gmail.com>
2024-09-24 15:10:28 -0300

    commit inicial

---
+++ myapp/views.py
@@ -0,0 +1,47 @@
+from django.http import HttpResponse
+from .models import Project, Task
+from django.shortcuts import render, redirect
+from .forms import CreateNewTask, CreateNewProject
+
+# Create your views here.
+
+def index(request):
+    title = 'django course!!'
+    return render(request,'index.html',{
+        'title': title
+    })
+
+def about(request):
+    username = 'SMonjes123'
+    return render(request,'about.html',{
+        'username': username
+    })
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s</h1>" % username)
+
+def projects (request):
+    #projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request,'projects/projects.html',{
+        'projects':projects
+    })
+
+def tasks (request):
+    # task = Task.objects.get(title=title) 
+    tasks = Task.objects.all()
+    return render(request,'tasks/tasks.html', {
+        'tasks': tasks
+    })
+
+def create_task(request): 
+    if request.method == 'GET':
+        return render(request,'create_task.html', {
+            'form' : CreateNewTask()
+        })
+    else:
+        Task.objects.create(title=request.POST['title'], descripcion=request.POST['descripcion'], project_id=2)  
+        return redirect('/tasks/')
+    
+def create_project(request):
+    return render(request, 'projects/create_project.html')
